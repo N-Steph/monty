d71dede03a8bc010f73ecff4b72c2781a671f54a
N-Steph <snouaffo@gmail.com>
2023-10-09 07:10:08 +0000

    Added the prototype of clean_up_handler_fail() function

--- monty.h
+++ monty.h
@@ -57,6 +57,7 @@ void error_push(size_t line_number);
 void free_stack(stack_t **stack);
 int validate_arg(size_t ln, char **opcode_read);
 void clean_up(stack_t **stack, char *ptr, FILE *fileptr);
+void clean_up_handler_fail(stack_t **stack, FILE *fileptr);
 size_t dlistint_len(stack_t *stack);
 
 #endif /* MONTY_H */

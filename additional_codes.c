bbc781fb4bf2782d58b807c24b14cfbc8d529901
N-Steph <snouaffo@gmail.com>
2023-10-10 01:55:03 +0000

    move the add() function to arithmetic_code.c file

--- additional_codes.c
+++ additional_codes.c
@@ -14,30 +14,3 @@ void nop(stack_t **stack, unsigned int __attribute__((unused)) line_number)
 	if (line_number != 0)
 		return;
 }
-
-/**
- * add - adds the top two elements of the stack
- * @stack: stack containing value to print
- * @line_number: line at which opcode was read
- *
- * Return: nothing
- */
-void add(stack_t **stack, unsigned int __attribute__((unused)) line_number)
-{
-	stack_t *temp;
-	size_t len;
-
-	if (stack == NULL)
-		return;
-	len = dlistint_len(*stack);
-	if (len < 2)
-	{
-		dprintf(STDERR_FILENO, "L%d: can't add, stack too short\n", line_number);
-		errno = -1;
-		return;
-	}
-	(*stack)->prev->n = (*stack)->n + (*stack)->prev->n;
-	temp = *stack;
-	*stack = (*stack)->prev;
-	free(temp);
-}

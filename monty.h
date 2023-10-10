0e42b8f9c860a3ced1020206214ab1d0c31e8640
N-Steph <snouaffo@gmail.com>
2023-10-10 07:47:26 +0000

    Added prototype of pstr() function

--- monty.h
+++ monty.h
@@ -58,6 +58,7 @@ void mul(stack_t **stack, unsigned int line_number);
 void mod(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void pchar(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
 void error_push(size_t line_number);
 void free_stack(stack_t **stack);
 void clean_up_selector(int status, stack_t **stack, char *ptr, FILE *fptr);

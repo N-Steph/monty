56a27710c94931a52114fba8dbfd67be713e8f20
N-Steph <snouaffo@gmail.com>
2023-10-10 01:59:25 +0000

    Deleted validate_opcode() function definition

--- execute_opcode.c
+++ execute_opcode.c
@@ -38,9 +38,6 @@ int execute_opcode(stack_t **stack, char *ptr, size_t line_number)
 		}
 		integer = atoi(opcode_read[1]);
 	}
-	status = validate_arg(line_number, opcode_read);
-	if (status != 1)
-		return (-1);
 	free(ptr);
 	error_code = errno;
 	handler(stack, line_number);
@@ -59,15 +56,11 @@ int execute_opcode(stack_t **stack, char *ptr, size_t line_number)
  */
 int check_opcode(char *ptr, size_t ln, char **opcode_read)
 {
-	char *opcode_list[8] = {"push", "pall", "pint",
-		"pop", "swap", "add", "nop", NULL};
+	char *opcode_list[] = {"push", "pall", "pint",
+		"pop", "swap", "add", "nop", "sub",
+		"div", "mul", "mod", NULL};
 	int i;
 
-	if (opcode_read[2] != NULL)
-	{
-		error_opcode(ptr, ln);
-		return (-1);
-	}
 	i = 0;
 	while (opcode_list[i] != NULL)
 	{
@@ -106,25 +99,4 @@ void error_opcode(char *ptr, size_t line_number)
 void error_push(size_t line_number)
 {
 	dprintf(STDERR_FILENO, "L%ld: usage: push integer\n", line_number);
-}
-
-/**
- * validate_arg - handles argument given to opcode
- *
- * Description: prints error message in case opcode is given argument
- * meanwhile it should not
- *
- * @ln: line at which opcode is
- * @opcode_read: array containing opcode and/or data
- *
- * Return: 1 (if argumen is valide), -1 otherwise
- */
-int validate_arg(size_t ln, char **opcode_read)
-{
-	if (strcmp(opcode_read[0], "push") == 0)
-		return (1);
-	if (opcode_read[1] == NULL)
-		return (1);
-	dprintf(STDERR_FILENO, "L%ld: usage: %s\n", ln, opcode_read[0]);
-	return (-1);
 }

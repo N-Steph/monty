7cd6ef66dfb63ea770bdd1444b441054e1449a34
N-Steph <snouaffo@gmail.com>
2023-10-11 01:45:11 +0000

    update

--- monty.h
+++ monty.h
@@ -39,14 +39,15 @@ typedef struct instruction_s
 } instruction_t;
 
 typedef void(*handler)(stack_t **stack, unsigned int line_number);
-handler opcode_handler_selector(char *opcode);
+handler opcode_handler_selector(char *opcode, int ds);
 
 int integer;
-int execute_opcode(stack_t **stack, char *ptr, size_t line_number);
+int execute_opcode(stack_t **stack, char **ptr, size_t line_num, int ds);
 void opcode_extractor(char *ptr, char **opcode_read);
 int check_opcode(char *ptr, size_t ln, char **opcode_read);
 void error_opcode(char *ptr, size_t line_number);
-void push(stack_t **stack, unsigned int line_number);
+void push_stack(stack_t **stack, unsigned int line_number);
+void push_queue(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void pop(stack_t **stack, unsigned int line_number);
@@ -63,9 +64,9 @@ void rotl(stack_t **stack, unsigned int line_number);
 void rotr(stack_t **stack, unsigned int line_number);
 void error_push(size_t line_number);
 void free_stack(stack_t **stack);
+int ds_format_selector(char *ptr);
 void clean_up_selector(int status, stack_t **stack, char *ptr, FILE *fptr);
 void clean_up(stack_t **stack, char *ptr, FILE *fileptr);
-void clean_up_handler_fail(stack_t **stack, FILE *fileptr);
 size_t dlistint_len(stack_t *stack);
 
 #endif /* MONTY_H */
